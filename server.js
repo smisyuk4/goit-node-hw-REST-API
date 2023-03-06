7e7259b6546b4e90581f2f87d5750212c1a4afb6
Sergey <smisyuk@gmail.com>
2023-03-06 21:36:44 +0200

    first connect to DB

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
 const app = require('./app')
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const client = new MongoClient(process.env.MONGO_URL);
+const dbName = 'db-contacts';
+
+const start = async () => {
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('contacts');
+
+  const contacts = await collection.find({}).toArray()
+  console.log(contacts)
+
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running. Use our API on port: ${process.env.PORT}`)
+  })
+
+  return 'done.';
+}
+
+start()
+
+
