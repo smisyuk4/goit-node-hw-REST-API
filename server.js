944b0ac4b6a07bf0e309249ee8f24d6202d01762
Sergiy Misyuk <smisyuk@gmail.com>
2023-04-06 23:59:25 +0300

    fix connection to base

--- server.js
+++ server.js
@@ -29,24 +29,18 @@ app.use(errorMiddleware)
 const PORT = process.env.PORT || 3000;
 const uriDb = process.env.MONGO_URI;
 
-mongoose.connect(uriDb)
-app.listen(PORT, function () {
-        console.log(`Server running. Use our API on port: ${PORT}`);
-      });
+mongoose.Promise = global.Promise;
 
+const connection = mongoose.connect(uriDb)
 
-// const connection = mongoose.connect(uriDb, {
-//   dbName: 'db-contacts',
-// });
-// const connection = mongoose.connect(uriDb)
-// mongoose.Promise = global.Promise;
+connection
+  .then(() => {
+    console.log(`Connection to DB done!`);
 
-// connection
-//   .then(() => {
-//     app.listen(PORT, function () {
-//       console.log(`Server running. Use our API on port: ${PORT}`);
-//     });
-//   })
-//   .catch(err =>
-//     console.log(`Server not running. Error message: ${err.message}`),
-//   );
+    app.listen(PORT, function () {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch(err =>
+    console.log(`Server not running. Error message: ${err.message}`),
+  );
